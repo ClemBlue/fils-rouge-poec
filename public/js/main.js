3bdd4016942227c208e583521d9eef7a0db5d091
RobinbroustailPro <git.robin.broustail@gmail.com>
2023-12-05 16:27:54 +0100

    Cookies 1er commit

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,28 @@
-/* Votre JavaScript ici */
+
+
+function ouvrirBoiteModale () {
+    
+
+    var boiteModale = document.getElementById('maBoiteModale');
+    boiteModale.style.display = 'block';
+};
+
+
+function fermerBoiteModale () {
+    var boiteModale = document.getElementById('maBoiteModale');
+    boiteModale.style.display = 'none';
+};
+
+//function isCookiesAccepted() {
+   // console.log (document.cookie.indexOf('cookies_accepted=true'));
+//}
+   // if (isCookiesAccepted()) {
+   // if (isCookiesAccepted()) {
+        // Code à exécuter si les cookies sont acceptés
+    //    console.log("Les cookies sont acceptés!");
+   // } else {
+        // Code à exécuter si les cookies ne sont pas acceptés
+     //   console.log("Les cookies ne sont pas acceptés.");
+   // }
+
+
