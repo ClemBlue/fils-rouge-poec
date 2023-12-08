a98e5253200a730dca2de05fc36f23d5187a4f75
RobinbroustailPro <git.robin.broustail@gmail.com>
2023-12-08 16:55:02 +0100

    il manque que la gestion du refus

--- public/js/main.js
+++ public/js/main.js
@@ -1,28 +1,63 @@
 
+function getCookie(cookieName) {
+    var name = cookieName + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    console.log('>>>>',decodedCookie);
+    var cookieArray = decodedCookie.split(';');
 
+    for (var i = 0; i < cookieArray.length; i++) {
+        var cookie = cookieArray[i].trim();
+        if (cookie.indexOf(name) == 0) {
+            return cookie.substring(name.length, cookie.length);
+        }
+    }
+
+    return null;
+}
 function ouvrirBoiteModale () {
     
 
     var boiteModale = document.getElementById('maBoiteModale');
-    boiteModale.style.display = 'block';
+    //boiteModale.style.display = 'block';
+    if (getCookie('tata')!=""){
+       
+        var boiteModale = document.getElementById('maBoiteModale');
+       boiteModale.style.display = 'none';
+       
+    }
+    else {
+        var boiteModale = document.getElementById('maBoiteModale');
+        boiteModale.style.display = 'block';
+        
+    }
+    
 };
 
 
 function fermerBoiteModale () {
     var boiteModale = document.getElementById('maBoiteModale');
     boiteModale.style.display = 'none';
+    setCookie('tata','666', 24);
 };
+function setCookie(cookieName, cookieValue, expirationDays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (expirationDays * 24 * 60 * 60 * 1000));
+    var expires = "expires=" + d.toUTCString();
+    document.cookie = cookieName + "=" + cookieValue + ";" + expires + ";path=/";
+}
+function getCookie(cookieName) {
+    var name = cookieName + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    console.log('>>>>',decodedCookie);
+    return decodedCookie;
+}
 
-//function isCookiesAccepted() {
-   // console.log (document.cookie.indexOf('cookies_accepted=true'));
-//}
-
-   // if (isCookiesAccepted()) {
-        // Code à exécuter si les cookies sont acceptés
-    //    console.log("Les cookies sont acceptés!");
-   // } else {
-        // Code à exécuter si les cookies ne sont pas acceptés
-     //   console.log("Les cookies ne sont pas acceptés.");
-   // }
+// Exemple : Lire la valeur du cookie "monCookie"
+var valeurDuCookie = getCookie("monCookie");
+if (valeurDuCookie) {
+    console.log("La valeur du cookie monCookie est : " + valeurDuCookie);
+} else {
+    console.log("Le cookie monCookie n'existe pas.");
+}
 
 
--- src/Controller/FirstController.php
+++ src/Controller/FirstController.php
@@ -5,12 +5,17 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class FirstController extends AbstractController
 {
     #[Route('/', name: 'accueil')]
     public function index(): Response
+
     {
         return $this->render('base.html.twig');
+        
+
+        }
     }
-}
+
